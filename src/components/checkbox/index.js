d3013439e16c92e1f0548b312c9b6ee7991f7771
En-Tilza <maarkov@mail.ru>
2020-04-28 12:21:27 +0300

    Checkbox. Add default state

--- src/components/checkbox/index.js
+++ src/components/checkbox/index.js
@@ -7,18 +7,26 @@ export default class Checkbox extends Component {
     constructor(props) {
         super(props);
 
+        this.state = {
+            checked: false
+        }
+
         this.onChange = this.onChange.bind(this);
     }
     onChange(event) {
+        let checked = event.target.checked;
         if( this.props.func )
-            this.props.func(event.target.checked);
+            this.props.func(checked);
+
+
+        this.setState({checked});
     }
     render() {
-        const { name, children, checked } = this.props;
+        const { name, children } = this.props;
 
         return(
             <label className="checkbox">
-                <input name={name} type="checkbox" className="checkbox__input" onChange={this.onChange} checked={checked} />
+                <input name={name} type="checkbox" className="checkbox__input" onChange={this.onChange} defaultChecked={this.props.checked}/>
                 <span className="checkbox__text">{children}</span>
             </label>
         )
