3524b3a726bf62c7124ae5bfe367803937cc1c64
En-Tilza <maarkov@mail.ru>
2020-04-28 13:10:11 +0300

    run script build

---
+++ build/precache-manifest.e57009716a89a4f03af0029061b6edf6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3c3155caa4593a7fd8b58a1fd7045fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04838187f38484300954",
+    "url": "/static/css/main.39853dc2.chunk.css"
+  },
+  {
+    "revision": "119b18d1d9f722cac1c7",
+    "url": "/static/js/2.296ae97d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.296ae97d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04838187f38484300954",
+    "url": "/static/js/main.8cd70794.chunk.js"
+  },
+  {
+    "revision": "6245f549f4f9249b69f4",
+    "url": "/static/js/runtime-main.3ae6056e.js"
+  },
+  {
+    "revision": "3217df059f9026fa5635927461715f27",
+    "url": "/static/media/chef-famale.3217df05.svg"
+  },
+  {
+    "revision": "2c338c6c065f20ddb23ca5a4b8df78bc",
+    "url": "/static/media/chef-male.2c338c6c.svg"
+  },
+  {
+    "revision": "684cf63390a0846207a1c83e11ecbb82",
+    "url": "/static/media/driver-famale.684cf633.svg"
+  },
+  {
+    "revision": "3cf59282eab659c93d573eb57041bff4",
+    "url": "/static/media/driver-male.3cf59282.svg"
+  },
+  {
+    "revision": "46617dfa3448eed13f96e730975ea959",
+    "url": "/static/media/waiter.46617dfa.svg"
+  },
+  {
+    "revision": "67d6c79fd73c39b09bc864ce9d770209",
+    "url": "/static/media/waitres.67d6c79f.svg"
+  }
+]);
